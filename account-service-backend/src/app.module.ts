36f472ef06f2b0e86c6c97af581e7883b1867671
Jiaxuan He <jiaxuan_he@yahoo.de>
2020-09-03 12:26:13 +0200

    minor bug fix

--- account-service-backend/src/app.module.ts
+++ account-service-backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module, HttpModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ServiceRegistrationController } from './service-registration/service-registration.controller';
 
 @Module({
   imports: [HttpModule],
-  controllers: [AppController, ServiceRegistrationController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
